7db6dc0c9c431e81dd609d3e2f758e6f6843a85b
Robin Genz <mail@robingenz.dev>
2026-04-30 14:39:47 +0200

    Initial commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Robin Genz on 30.04.26.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
